609034b4a7d8590c5b410075c1c9aae65bf71fef
Lucia Mendiguren <mendiguren.lucia@gmail.com>
2015-04-11 23:03:23 -0400

    Cambios a la pagina

---
+++ dogbarklaststep.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+def dogtranslator(context, dogsize):
+    
+    columnasContext=np.array([ball, stranger, fight, walk, play, alone ])
+    filasDogsize=np.array([big,medium,small])
+    
+
+    return sentence
